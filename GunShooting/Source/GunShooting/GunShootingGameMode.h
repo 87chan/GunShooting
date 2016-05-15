696408133e17a1fb8955c4b258e90d381fa95932
87chan <honji31415@gmail.com>
2016-05-16 00:29:13 +0900

    ファイルの追加

---
+++ GunShooting/Source/GunShooting/GunShootingGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "GunShootingGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GUNSHOOTING_API AGunShootingGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
